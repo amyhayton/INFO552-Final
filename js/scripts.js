3d3b4d7d913d965a4b533b9c00f920bcee973ee0
amyhayton <aeh372@drexel.edu>
2024-06-14 18:00:45 -0400

    added hero

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+var burgerBtn = document.querySelector("#burger-container button");
+
+var mainNavContainer = document.querySelector("#main-nav");
+
+var mainNavVisible = false;
+
+burgerBtn.addEventListener("click", () =>{
+    console.log("burger clicked");
+
+    if(mainNavVisible === false){
+
+        mainNavContainer.classList.remove("hideMainNav");
+        mainNavVisible = true;
+
+    }else{
+
+        mainNavContainer.classList.add("hideMainNav");
+        mainNavVisible = false;
+    }
+})
+
+mainNavContainer.addEventListener("click", function(){
+    mainNavContainer.classList.add("hideMainNav");
+    mainNavVisible = false;
+})
